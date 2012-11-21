daf7058b1226158ccc04dca4f7d36e507ec1183e
Rob Griffiths <rob@bytespider.eu>
2012-11-21 16:00:46 +0000

    added better handling of start, stop, and pause

--- downtime.js
+++ downtime.js
@@ -1,3 +1,32 @@
+/*!
+ * jQuery downtime plugin
+ * version 0.2
+ * Author: Rob Griffiths <rob@bytespider.eu> http://github.com/bytespider/downtime
+ * Licence: MIT license
+*/
+
+/*
+ * Copyright (c) 2012 Rob Griffiths
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 !function($) {
     $.fn.downtime = function (method) {
 
@@ -36,8 +65,8 @@
         var methods = {
             init: function (options) {
                 options = $.extend({
-                    time: 60000,
-                    intervalFequency: 1000 /* 1 second */,
+                    time: null,
+                    intervalFrequency: 1000 /* 1 second */,
                     type: 'countdown' /* countup */,
                     autostart: true,
                     complete: null,
@@ -46,9 +75,14 @@
                     secondsFormat: timeFormat,
                 }, options);
 
+                if (null != options.time) {
+                    $.error('missing option time passed to jQuery.downtime');
+                    return this;
+                }
+
                 if (!/^count(up|down)$/.test(options.type)) {
-                     $.error('type ' +  options.type + ' is not a valid jQuery.downtime count type');
-                     return this;
+                    $.error('type ' +  options.type + ' is not a valid jQuery.downtime count type');
+                    return this;
                 }
 
                 this.each(function () {
@@ -58,11 +92,14 @@
                     count_types[options.type].init($this);
 
                     var time = $this.data('time');
-                    $this.bind('timer.update', update).trigger('timer.update', {
+                    var intervalFrequency = parseInt($this.data('options').intervalFrequency, 10);
+
+                    $this.bind('downtime.update', update).trigger('downtime.update', {
                         item: $this,
-                        hours: hours(time),
-                        minutes: minutes(time),
-                        seconds: seconds(time)
+                        hours: hours(time, intervalFrequency),
+                        minutes: minutes(time, intervalFrequency),
+                        seconds: seconds(time, intervalFrequency),
+                        time: time
                     });
                 });
 
@@ -72,17 +109,34 @@
 
                 return this;
             },
+            option: function (option, value) {
+                var options = $(this).data('options') || {};
+
+                if (null != value) {
+                    // set
+                    options[option] = value;
+                    return $(this).data('options', options);
+                }
+
+                // get
+                return option in options ? options[option] : null;
+            },
             start: function () {
                 return this.each(function () {
                     $this = $(this);
 
-                    var intervalFequency = parseInt($this.data('options').intervalFequency, 10);
+                    if ($this.data('timeout_id')) {
+                        // already running
+                        return;
+                    }
+
+                    var intervalFrequency = parseInt($this.data('options').intervalFrequency, 10);
                     var timeout_id = setInterval(function () {
                         var options = $this.data('options');
                         var time = count_types[options.type].update($this);
 
                         if (count_types[options.type].finished($this)) {
-                            $this.trigger('timer.complete', {
+                            $this.trigger('downtime.complete', {
                                 item: $this
                             });
 
@@ -93,20 +147,23 @@
                             methods.stop.call($this);
                         }
 
-                        $this.trigger('timer.update', {
+                        $this.trigger('downtime.update', {
                             item: $this,
-                            hours: hours(time),
-                            minutes: minutes(time),
-                            seconds: seconds(time)
+                            hours: hours(time, intervalFrequency),
+                            minutes: minutes(time, intervalFrequency),
+                            seconds: seconds(time, intervalFrequency),
+                            time: time
                         });
 
-                    }, intervalFequency);
+                    }, intervalFrequency);
                     $this.data('timeout_id', timeout_id);
                 });
             },
             pause: function () {
                 return this.each(function () {
-                    clearInterval($(this).data('timeout_id'));
+                    if ($(this).data('timeout_id')) {
+                        clearInterval($(this).data('timeout_id'));
+                    }
                 });
             },
             stop: function () {
@@ -114,7 +171,9 @@
                     $this = $(this);
 
                     $this.data('time', parseInt($this.data('options').time, 10));
-                    clearInterval($this.data('timeout_id'));
+                    if ($(this).data('timeout_id')) {
+                        clearInterval($this.data('timeout_id'));
+                    }
                 });
             },
             show: function () {
@@ -140,16 +199,16 @@
         };
 
 
-        function hours(timestamp) {
-            return Math.floor(timestamp / 3600);
+        function hours(timestamp, resolution) {
+            return Math.floor(timestamp / resolution / 3600);
         }
 
         function minutes(timestamp) {
-            return Math.floor((timestamp / 60) % 60);
+            return Math.floor((timestamp / resolution / 60) % 60);
         }
 
         function seconds(timestamp) {
-            return Math.floor(timestamp % 60);
+            return Math.floor(timestamp / resolution % 60);
         }
 
         function update(event, data) {
